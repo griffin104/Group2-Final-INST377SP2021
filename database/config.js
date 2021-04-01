6a62206232ba3a39a85c28d525574044e844e59b
Joseph Sanchez <sanchezjoseph378@gmail.com>
2021-04-01 13:45:55 -0400

    ADD: CONFIG.JS Index, and Players model

---
+++ database/config.js
@@ -0,0 +1,23 @@
+export default {
+  development: {
+    username: 'student',
+    password: 'INST377@UMD',
+    database: 'maryland_bball',
+    host: '3.236.243.212',
+    dialect: 'mysql'
+  },
+  test: {
+    username: 'student',
+    password: 'INST377@UMD',
+    database: 'maryland_bball',
+    host: '3.236.243.212',
+    dialect: 'mysql'
+  },
+  production: {
+    username: 'student',
+    password: 'INST377@UMD',
+    database: 'maryland_bball',
+    host: '3.236.243.212',
+    dialect: 'mysql'
+  }
+};
