7d860e592766c76da84d417a2a98204461b38378
Joseph Sanchez <sanchezjoseph378@gmail.com>
2021-04-07 22:30:44 -0400

    EDIT: Organized The 5 model endpoints

--- src/server/routes/apiRoutes.mjs
+++ src/server/routes/apiRoutes.mjs
@@ -10,8 +10,33 @@ router.get("/", (req, res) => {
 })
 
 
-// PLAYERS
+  /////////////////// /////////////////// ///////////////////
+/////////////////// PLAYER GAME STATS ///////////////////
+/////////////////// /////////////////// ///////////////////
 
+router.get("/player_game_stats", async (req, res) => {
+  try {
+    const stats = await db.PlayerGameStats.findAll();
+    const reply =
+      stats.length > 0 ? { data: stats } : { message: "no results found" };
+    res.json(reply);
+  } catch (err) {
+    console.error(err);
+    res.error("Server error");
+  } 
+});
+
+router.get("/player_game_stats:game_stats_id", async (req, res) => {
+  try {
+    //ENDPOINT to get ELEMENT by ID
+  } catch (err) {
+    console.error(err);
+    res.error("Server Error");
+});
+
+/////////////////// /////////////////// ///////////////////
+/////////////////// PLAYERS_BIOS ///////////////////
+/////////////////// /////////////////// ///////////////////
 router.get("/players", async (req, res) => {
     try {
       const stats = await db.Players.findAll();
@@ -24,9 +49,9 @@ router.get("/players", async (req, res) => {
     } 
   });
 
-
-// GAMES
-
+/////////////////// /////////////////// ///////////////////
+/////////////////// GAMES ///////////////////
+/////////////////// /////////////////// ///////////////////
 router.get("/games", async (req, res) => {
     try {
       const stats = await db.Games.findAll();
@@ -39,18 +64,57 @@ router.get("/games", async (req, res) => {
     } 
   });
 
-// PLAYER GAME STATS
 
-router.get("/player-game-stats", async (req, res) => {
+  router.get("/game_stats:game_stats_id", async (req, res) => {
+    try {
+      // ENDPOINT to get ELEMENT by ID
+    } catch (err) {
+      console.error(err);
+      res.error("Server Error");
+  });
+
+
+/////////////////// /////////////////// ///////////////////
+/////////////////// Game Stats END POINTS ///////////////////
+/////////////////// /////////////////// ///////////////////
+router.get("/game_stats", async (req, res) => {
   try {
-    const stats = await db.PlayerGameStats.findAll();
-    const reply =
-      stats.length > 0 ? { data: stats } : { message: "no results found" };
-    res.json(reply);
+    const gameStats = await db.gameStats.findAll();
+    res.send(gameStats);
   } catch (err) {
     console.error(err);
-    res.error("Server error");
-  } 
+    res.error("Server Error");
+  }
+});
+
+router.get("/game_stats:game_stats_id", async (req, res) => {
+  try {
+    // ENDPOINT to get ELEMENT by ID
+  } catch (err) {
+    console.error(err);
+    res.error("Server Error");
+});
+
+
+/////////////////// /////////////////// ///////////////////
+/////////////////// Player Stats END POINTS ///////////////////
+/////////////////// /////////////////// ///////////////////
+router.get("/player_stats", async (req, res) => {
+  try {
+    const playerStats = await db.playerStats.findAll();
+    res.send(playerStats);
+  } catch (err) {
+    console.error(err);
+    res.error("Server Error");
+  }
+});
+
+router.get("/player_stats:player_stats_id", async (req, res) => {
+  try {
+    // ENDPOINT to get ELEMENT by ID
+  } catch (err) {
+    console.error(err);
+    res.error("Server Error");
 });
 
 
