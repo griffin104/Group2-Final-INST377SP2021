4de0bd3161ac37a440fa08f41a406039b2a0be15
vigneshkvishwanathan <vigneshkvishwanathan@gmail.com>
2021-04-15 17:56:05 -0400

    Update PlayerBios.mjs

--- src/server/models/PlayerBios.mjs
+++ src/server/models/PlayerBios.mjs
@@ -2,6 +2,60 @@ export default (sequelize, DataTypes) => {
   const PlayerBios = sequelize.define(
       "player_bios",
       {
+          player_bios_id: {
+              type: DataTypes.INTEGER,
+              allowNull: false,
+              autoIncrement: true,
+              primaryKey: true
+          },
+          player_id: {
+              type: DataTypes.INTEGER,
+              allowNull: false
+          },
+          player_number: {
+              type: DataTypes.INTEGER,
+              allowNull: false
+          },
+          first_name: {
+              type: DataTypes.VARCHAR,
+              allowNull: false
+          },
+          last_name: {
+              type: DataTypes.VARCHAR,
+              allowNull: false
+          },
+          height_feet: {
+              type: DataTypes.INTEGER,
+              allowNull: false
+          },
+          height_inches: {
+              type: DataTypes.INTEGER,
+              allowNull: false
+          },
+          weight: {
+              type: DataTypes.INTEGER,
+              allowNull: false
+          },
+          hometown_city: {
+              type: DataTypes.VARCHAR,
+              allowNull: false
+          },
+          hometown_state: {
+              type: DataTypes.VARCHAR,
+              allowNull: false
+          },
+          major: {
+              type: DataTypes.VARCHAR,
+              allowNull: false
+          },
+          high_school: {
+              type: DataTypes.VARCHAR,
+              allowNull: false
+          },
+          season: {
+              type: DataTypes.INTEGER,
+              allowNull: false
+          }
           
 
       },
